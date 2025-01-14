6b4d9c79f30b01cc02efa76a1f609a4fb180d7c1
George <106427405+6ogo@users.noreply.github.com>
2025-01-14 18:10:47 +0100

    Update results_page.dart

--- fridgeai/lib/results_page.dart
+++ fridgeai/lib/results_page.dart
@@ -1,39 +1,10 @@
 import 'package:flutter/material.dart';
-import 'dart:io';
-import 'api_service.dart';
 import 'recipe_details_page.dart';
 
-class ResultsPage extends StatefulWidget {
-  final File image;
+class ResultsPage extends StatelessWidget {
+  final List<dynamic> recipes;
 
-  const ResultsPage({super.key, required this.image});
-
-  @override
-  ResultsPageState createState() => ResultsPageState();
-}
-
-class ResultsPageState extends State<ResultsPage> {
-  List<String>? ingredients;
-  List<dynamic>? recipes;
-  bool isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _analyzeImage();
-  }
-
-  Future<void> _analyzeImage() async {
-    // Send the image to the API for analysis
-    final detectedIngredients = await ApiService.analyzeImage(widget.image);
-    final recipeSuggestions = await ApiService.fetchRecipes(detectedIngredients);
-
-    setState(() {
-      ingredients = detectedIngredients;
-      recipes = recipeSuggestions;
-      isLoading = false;
-    });
-  }
+  const ResultsPage({super.key, required this.recipes});
 
   @override
   Widget build(BuildContext context) {
@@ -41,33 +12,24 @@ class ResultsPageState extends State<ResultsPage> {
       appBar: AppBar(
         title: const Text('Recipe Suggestions'),
       ),
-      body: isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                Text('Detected Ingredients: ${ingredients!.join(", ")}'),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: recipes!.length,
-                    itemBuilder: (context, index) {
-                      final recipe = recipes![index];
-                      return ListTile(
-                        title: Text(recipe['label']),
-                        subtitle: Text('Ingredients: ${recipe['ingredientLines'].join(", ")}'),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => RecipeDetailsPage(recipe: recipe),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  ),
+      body: ListView.builder(
+        itemCount: recipes.length,
+        itemBuilder: (context, index) {
+          final recipe = recipes[index];
+          return ListTile(
+            title: Text(recipe['title']),
+            subtitle: Text('Used Ingredients: ${recipe['usedIngredientCount']}'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => RecipeDetailsPage(recipeId: recipe['id']),
                 ),
-              ],
-            ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
