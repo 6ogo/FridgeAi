ee6f7c7804f0c14c19b909c0bc64e5af7eacbd12
George <106427405+6ogo@users.noreply.github.com>
2025-01-14 17:55:53 +0100

    Update recipe_model.dart

--- fridgeai/lib/recipe_model.dart
+++ fridgeai/lib/recipe_model.dart
@@ -1,29 +1,19 @@
-import 'package:flutter/material.dart';
+class Recipe {
+  final String label;
+  final List<String> ingredientLines;
+  final String url;
 
-class RecipeDetailsPage extends StatelessWidget {
-  final dynamic recipe;
+  Recipe({
+    required this.label,
+    required this.ingredientLines,
+    required this.url,
+  });
 
-  const RecipeDetailsPage({super.key, required this.recipe});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(recipe['label']),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('Ingredients:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            ...recipe['ingredientLines'].map((ingredient) => Text(ingredient)).toList(),
-            const SizedBox(height: 20),
-            const Text('Instructions:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            Text(recipe['url'] ?? 'No instructions available.'),
-          ],
-        ),
-      ),
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(
+      label: json['label'],
+      ingredientLines: List<String>.from(json['ingredientLines']),
+      url: json['url'] ?? 'No instructions available.',
     );
   }
 }
