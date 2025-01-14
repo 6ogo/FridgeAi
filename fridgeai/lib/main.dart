b94b8b6252bcc2779f4eebed9a86134cfa698f7d
George <106427405+6ogo@users.noreply.github.com>
2025-01-14 19:36:56 +0100

    Update main.dart

--- fridgeai/lib/main.dart
+++ fridgeai/lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'home_page.dart';
+import 'dart:io';
+
 
 void main() async {
-  await dotenv.load(fileName: "../.env");
+  if (kDebugMode) {
+    print("Current directory: ${Directory.current.path}");
+  } // Debug the working directory
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
